ba0b353a8673c9372cc264b4f4fc1fbf4303156a
Tom Berman <tjcberman@gmail.com>
2014-11-14 07:46:31 +0000

    finsished admin

--- client/templates/posts/admin.js
+++ client/templates/posts/admin.js
@@ -1,7 +1,33 @@
 Template.admin.helpers({ posts: function() {
-return Posts.find(); }
+return Posts.find(); },
+
+isChecked: function(){
+	return this.show;
+}
+});
+
+Template.admin.events({
+	"click .chkbox": function (event, template) {
+		var state = $('#'+this._id).prop('checked');
+		var eid = this._id;
+		
+		Posts.update({_id:eid},{$set: {show:state}});
+		
+		
+		
+		}
 });
 
 Template.admin.rendered = function(){
 	$("img.lazy").lazyload();
 };
+
+
+Posts.allow({
+	insert: function () {
+		return true;
+	},
+	update: function () {
+		return true;
+	}
+});
