38ea6329f17cf4e210fd11e696994f542d2b3103
Tom Berman <tjcberman@gmail.com>
2015-05-07 19:46:01 +0100

    alpha ver

---
+++ client/templates/application/tmpHome.js
@@ -0,0 +1,3 @@
+Template.tmpHome.rendered = function(){
+	imageMapResize('#Map');
+};
--- client/templates/work/nav.js
+++ client/templates/work/nav.js
@@ -8,6 +8,10 @@ points: function(){
 }
 });
 
+Template.nav.rendered = function(){
+	imageMapResize('#Map');
+};
+
 Template.nav.events({
 	'click .done':function(e,t){
 		Meteor.call('userScore', Session.get('user'),Session.get('points'), function(e,d){
